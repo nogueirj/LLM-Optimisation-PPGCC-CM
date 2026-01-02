ac699925e8f051a1ef1669125a23a87ba21b645f
Julio Nogueira <julio.nogueira@outlook.com>
2026-01-02 13:29:18 -0300

    Corrigindo o arquivo otimizado usando o prompt correto

--- codestral/3mm/3mm_openmp_codestral_optmized.c
+++ codestral/3mm/3mm_openmp_codestral_optmized.c
@@ -1,124 +1,166 @@
+/**
+ * 3mm.c: This file is part of the PolyBench/C 3.2 test suite.
+ *
+ *
+ * Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
+ * Web address: http://polybench.sourceforge.net
+ */
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <math.h>
 
 /* Include polybench common header. */
-#include "polybench.h"
+#include <polybench.h>
 
 /* Include benchmark-specific header. */
+/* Default data type is double, default size is 4000. */
 #include "3mm.h"
-#include <omp.h>  // Inclusão da biblioteca OpenMP
+#include <omp.h>
 
 /* Array initialization. */
 static
 void init_array(int ni, int nj, int nk, int nl, int nm,
-                DATA_TYPE *alpha,
-                DATA_TYPE *beta,
-                DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
-                DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
-                DATA_TYPE POLYBENCH_2D(C, NL, NM, nl, nm),
-                DATA_TYPE POLYBENCH_2D(D, NJ, NM, nj, nm))
+		DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk),
+		DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj),
+		DATA_TYPE POLYBENCH_2D(C,NJ,NM,nj,nm),
+		DATA_TYPE POLYBENCH_2D(D,NM,NL,nm,nl))
 {
-    int i, j;
+  int i, j;
 
-    *alpha = (DATA_TYPE) random();
-    *beta = (DATA_TYPE) random();
-    for (i = 0; i < ni; i++)
-        for (j = 0; j < nk; j++)
-            A[i][j] = ((DATA_TYPE) i*j + 2) / (ni * nk);
-    for (i = 0; i < nk; i++)
-        for (j = 0; j < nj; j++)
-            B[i][j] = ((DATA_TYPE) i*j - 1) / (nk * nj);
-    for (i = 0; i < nl; i++)
-        for (j = 0; j < nm; j++)
-            C[i][j] = ((DATA_TYPE) i*j + 3) / (nl * nm);
-    for (i = 0; i < nj; i++)
-        for (j = 0; j < nm; j++)
-            D[i][j] = ((DATA_TYPE) i*j - 5) / (nj * nm);
+  for (i = 0; i < ni; i++)
+    for (j = 0; j < nk; j++)
+      A[i][j] = ((DATA_TYPE) i*j) / ni;
+  for (i = 0; i < nk; i++)
+    for (j = 0; j < nj; j++)
+      B[i][j] = ((DATA_TYPE) i*(j+1)) / nj;
+  for (i = 0; i < nj; i++)
+    for (j = 0; j < nm; j++)
+      C[i][j] = ((DATA_TYPE) i*(j+3)) / nl;
+  for (i = 0; i < nm; i++)
+    for (j = 0; j < nl; j++)
+      D[i][j] = ((DATA_TYPE) i*(j+2)) / nk;
 }
 
-/* Main computation function. */
+
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
+static
+void print_array(int ni, int nl,
+		 DATA_TYPE POLYBENCH_2D(G,NI,NL,ni,nl))
+{
+  int i, j;
+
+  for (i = 0; i < ni; i++)
+    for (j = 0; j < nl; j++) {
+	fprintf (stderr, DATA_PRINTF_MODIFIER, G[i][j]);
+	if ((i * ni + j) % 20 == 0) fprintf (stderr, "\n");
+    }
+  fprintf (stderr, "\n");
+}
+
+
+/* Main computational kernel. The whole function will be timed,
+   including the call and return. */
 static
 void kernel_3mm(int ni, int nj, int nk, int nl, int nm,
-               DATA_TYPE alpha,
-               DATA_TYPE beta,
-               DATA_TYPE POLYBENCH_2D(E, NI, NL, ni, nl),
-               DATA_TYPE POLYBENCH_2D(F, NL, NJ, nl, nj),
-               DATA_TYPE POLYBENCH_2D(G, NI, NM, ni, nm),
-               DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
-               DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
-               DATA_TYPE POLYBENCH_2D(C, NL, NM, nl, nm),
-               DATA_TYPE POLYBENCH_2D(D, NJ, NM, nj, nm))
+		DATA_TYPE POLYBENCH_2D(E,NI,NJ,ni,nj),
+		DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk),
+		DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj),
+		DATA_TYPE POLYBENCH_2D(F,NJ,NL,nj,nl),
+		DATA_TYPE POLYBENCH_2D(C,NJ,NM,nj,nm),
+		DATA_TYPE POLYBENCH_2D(D,NM,NL,nm,nl),
+		DATA_TYPE POLYBENCH_2D(G,NI,NL,ni,nl))
 {
-    int i, j, k;
+  int i, j, k;
 
-#pragma omp parallel for private(i, j, k)
-    for (i = 0; i < _PB_NI; i++)
-        for (j = 0; j < _PB_NL; j++) {
-            E[i][j] = 0;
-            for (k = 0; k < _PB_NK; ++k)
-                E[i][j] += alpha * A[i][k] * B[k][j];
-        }
-
-#pragma omp parallel for private(i, j, k)
-    for (i = 0; i < _PB_NL; i++)
-        for (j = 0; j < _PB_NJ; j++) {
-            F[i][j] = 0;
-            for (k = 0; k < _PB_NM; ++k)
-                F[i][j] += E[i][k] * D[k][j];
-        }
-
-#pragma omp parallel for private(i, j, k)
-    for (i = 0; i < _PB_NI; i++)
-        for (j = 0; j < _PB_NM; j++) {
-            G[i][j] = 0;
-            for (k = 0; k < _PB_NJ; ++k)
-                G[i][j] += F[i][k] * C[k][j];
-        }
-
-#pragma omp parallel for private(i, j)
-    for (i = 0; i < _PB_NI; i++)
-        for (j = 0; j < _PB_NM; j++)
-            G[i][j] *= beta;
+#pragma omp parallel for private(j, k) shared(E, A, B, F, C, D, G) collapse(2)
+/* E := A*B */
+for (i = 0; i < _PB_NI; i++)
+for (j = 0; j < _PB_NJ; j++)
+{
+    E[i][j] = 0;
+    for (k = 0; k < _PB_NK; ++k)
+        E[i][j] += A[i][k] * B[k][j];
 }
 
-int main()
+#pragma omp parallel for private(i, k) shared(F, C, D) collapse(2)
+/* F := C*D */
+for (i = 0; i < _PB_NJ; i++)
+for (j = 0; j < _PB_NL; j++)
 {
-    /* Register polybench variables. */
-    int ni = NI, nj = NJ, nk = NK, nl = NL, nm = NM;
-    DATA_TYPE alpha;
-    DATA_TYPE beta;
-    POLYBENCH_2D_ARRAY_DECL(E, DATA_TYPE, NI, NL, ni, nl);
-    POLYBENCH_2D_ARRAY_DECL(F, DATA_TYPE, NL, NJ, nl, nj);
-    POLYBENCH_2D_ARRAY_DECL(G, DATA_TYPE, NI, NM, ni, nm);
-    POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, NI, NK, ni, nk);
-    POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, NK, NJ, nk, nj);
-    POLYBENCH_2D_ARRAY_DECL(C, DATA_TYPE, NL, NM, nl, nm);
-    POLYBENCH_2D_ARRAY_DECL(D, DATA_TYPE, NJ, NM, nj, nm);
+    F[i][j] = 0;
+    for (k = 0; k < _PB_NM; ++k)
+        F[i][j] += C[i][k] * D[k][j];
+}
 
-    /* Allocate memory. */
-    polybench_alloc_data(NI, NL, &E[0][0], &F[0][0], &G[0][0], &A[0][0], &B[0][0], &C[0][0], &D[0][0]);
+#pragma omp parallel for private(j, k) shared(E, F, G) collapse(2)
+/* G := E*F */
+for (i = 0; i < _PB_NI; i++)
+for (j = 0; j < _PB_NL; j++)
+{
+    G[i][j] = 0;
+    for (k = 0; k < _PB_NJ; ++k)
+        G[i][j] += E[i][k] * F[k][j];
+}
+}
 
-    /* Initialize array. */
-    init_array(ni, nj, nk, nl, nm, &alpha, &beta, A, B, C, D);
 
-    /* Start timer. */
-    polybench_start_instruments;
 
-    /* Run kernel. */
-    kernel_3d_convolution<DATA_TYPE>(ni, nj, nk, nl, nm, alpha, beta, E, F, G, A, B, C, D);
+int main(int argc, char** argv)
+{
+  /* Retrieve problem size. */
+  int ni = NI;
+  int nj = NJ;
+  int nk = NK;
+  int nl = NL;
+  int nm = NM;
 
-    /* Stop and print timer. */
-    polybench_stop_instruments;
-    polybench_print_instruments;
+  /* Variable declaration/allocation. */
+  POLYBENCH_2D_ARRAY_DECL(E, DATA_TYPE, NI, NJ, ni, nj);
+  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, NI, NK, ni, nk);
+  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, NK, NJ, nk, nj);
+  POLYBENCH_2D_ARRAY_DECL(F, DATA_TYPE, NJ, NL, nj, nl);
+  POLYBENCH_2D_ARRAY_DECL(C, DATA_TYPE, NJ, NM, nj, nm);
+  POLYBENCH_2D_ARRAY_DECL(D, DATA_TYPE, NM, NL, nm, nl);
+  POLYBENCH_2D_ARRAY_DECL(G, DATA_TYPE, NI, NL, ni, nl);
 
-    /* Prevent dead-code elimination. All live-out data must be printed
-       by the function call in argument. */
-    polybench_prevent_dce(print_array4);
+  /* Initialize array(s). */
+  init_array (ni, nj, nk, nl, nm,
+	      POLYBENCH_ARRAY(A),
+	      POLYBENCH_ARRAY(B),
+	      POLYBENCH_ARRAY(C),
+	      POLYBENCH_ARRAY(D));
 
-    /* Be clean. */
-    polybench_free_data(E, F, G, A, B, C, D);
+  /* Start timer. */
+  polybench_start_instruments;
 
-    return 0;
+  /* Run kernel. */
+  kernel_3mm (ni, nj, nk, nl, nm,
+	      POLYBENCH_ARRAY(E),
+	      POLYBENCH_ARRAY(A),
+	      POLYBENCH_ARRAY(B),
+	      POLYBENCH_ARRAY(F),
+	      POLYBENCH_ARRAY(C),
+	      POLYBENCH_ARRAY(D),
+	      POLYBENCH_ARRAY(G));
+
+  /* Stop and print timer. */
+  polybench_stop_instruments;
+  polybench_print_instruments;
+
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+  polybench_prevent_dce(print_array(ni, nl,  POLYBENCH_ARRAY(G)));
+
+  /* Be clean. */
+  POLYBENCH_FREE_ARRAY(E);
+  POLYBENCH_FREE_ARRAY(A);
+  POLYBENCH_FREE_ARRAY(B);
+  POLYBENCH_FREE_ARRAY(F);
+  POLYBENCH_FREE_ARRAY(C);
+  POLYBENCH_FREE_ARRAY(D);
+  POLYBENCH_FREE_ARRAY(G);
+
+  return 0;
 }
